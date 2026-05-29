f5564a46ea307ca3182c4f88717471ba25224f6e
Priyanshu Raj <rajanshu645@gmail.com>
2026-05-29 08:37:59 +0530

    message route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
 import {connectDB} from "./lib/db.js"
 import cookieParser from "cookie-parser"
@@ -9,8 +10,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use("/api/auth", authRoutes);
 app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("Server on",+PORT);
